3512c43a81f668e12209dc6a137d546f8dcafbd8
BJL156 <bjl15634@gmail.com>
2026-07-21 19:24:28 -0400

    Added the AST structure for Parser.

--- src/ast.h
+++ src/ast.h
@@ -35,4 +35,70 @@ struct Expr {
   };
 };
 
+typedef enum {
+  TYPE_INT,
+  TYPE_CHAR,
+  TYPE_VOID
+} Type;
+
+typedef struct {
+  Type base;
+  int pointer_depth;
+} TypeInfo;
+
+typedef enum {
+  STMT_EXPR,
+  STMT_VAR_DECL,
+  STMT_IF,
+  STMT_WHILE,
+  STMT_FOR,
+  STMT_RETURN,
+  STMT_BLOCK
+} StmtType;
+
+typedef struct Stmt Stmt;
+
+typedef struct {
+  Stmt **items;
+  int count;
+  int capacity;
+} StmtList;
+
+struct Stmt {
+  StmtType type;
+  union {
+    struct { Expr *expr; } expr_stmt;
+    struct { TypeInfo var_type; char name[64]; Expr *init; } var_decl;
+    struct { Expr *condition; Stmt *then_branch; Stmt *else_branch; } if_stmt;
+    struct { Expr *condition; Stmt *body; } while_stmt;
+    struct { Stmt *init; Expr *condition; Expr *increment; Stmt *body; } for_stmt;
+    struct { Expr *value; } return_stmt;
+    StmtList block;
+  };
+};
+
+typedef struct {
+  TypeInfo type;
+  char name[64];
+} Param;
+
+typedef struct {
+  Param *items;
+  int count;
+  int capacity;
+} ParamList;
+
+typedef struct {
+  TypeInfo return_type;
+  char name[64];
+  ParamList params;
+  Stmt *body;
+} Function;
+
+typedef struct {
+  Function *items;
+  int count;
+  int capacity;
+} Program;
+
 #endif
